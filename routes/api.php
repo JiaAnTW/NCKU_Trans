8e0467e18fa025a6a8bc6880ff989ebd37507558
alexsui <53047989+alexsui@users.noreply.github.com>
2020-11-24 22:00:58 +0800

    "add new QA line notify"

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::get('get/major', 'CommentsController@show');
 Route::middleware('commentNotify')->post('create/major','CommentsController@create');
 
 Route::get('get/major_QA', 'maj_QAController@show');
-Route::post('post/major_QA','maj_QAController@create');
+Route::middleware('QANotify')->post('post/major_QA','maj_QAController@create');
 
 Route::get('get/check', 'HomeController@check');
 
