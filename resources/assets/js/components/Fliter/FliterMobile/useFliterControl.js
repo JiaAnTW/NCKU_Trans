6dee554930c674998958df374d27bb6e5385fa75
JiaAnTW <at5lp6andy@gmail.com>
2020-12-05 14:03:02 +0800

    feat: init an useless useFliterControl

---
+++ resources/assets/js/components/Fliter/FliterMobile/useFliterControl.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+/* 資料結構
+var fliter= [
+    {
+        id:0,  ===> adjList的index
+        now:-1, 目前選取的item，預設為-1並顯示
+        name: "申請年",
+        type: "year",
+        option:[["全部年度",-1]]
+    },
+    {
+        id: 1,  adjList的index
+        now:-1, 目前選取的item，預設為-1並顯示
+        name: "申請年",
+        type: "year",
+        option:[["全部年度",-1]]
+    },
+];
+*/
+  
+
+function useFliterControl(initValue){
+    const [selectItem, setSelectItem] = useState({});
+
+    useEffect(()=>{
+        if(selectItem)
+            setData(selectItem);
+    },[selectItem]);
+
+    return { data, setData };
+}
+
+export default useFliterControl;
