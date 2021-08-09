7e136317170358a18bed0933f52683e778ea1263
JiaAnTW <at5lp6andy@gmail.com>
2021-08-09 20:37:15 +0800

    fix: call department and college only for once

--- resources/assets/js/page/Post/useInitOptions.js
+++ resources/assets/js/page/Post/useInitOptions.js
@@ -1,16 +1,18 @@
 import { useLayoutEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchDepartment } from '../../model/middleware/department';
 import { fetchCollege } from '../../model/middleware/college';
 import { useRequest } from '../../utils/index';
 
 function useInitOptions() {
     const dispatch = useDispatch();
+    const collegeData = useSelector((state) => state.college);
+    const departmentData = useSelector((state) => state.in_maj);
     const isFinishRequest = useRequest();
 
     useLayoutEffect(() => {
-        dispatch(fetchDepartment());
-        dispatch(fetchCollege());
+        if (departmentData.length === 0) dispatch(fetchDepartment());
+        if (collegeData.length === 0) dispatch(fetchCollege());
     }, []);
 
     return isFinishRequest;
