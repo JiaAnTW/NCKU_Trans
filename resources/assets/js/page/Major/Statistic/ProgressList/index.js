7a7e32c9d6ef4413996c49080040a850f2dea31f
JiaAnTW <at5lp6andy@gmail.com>
2021-08-12 00:21:57 +0800

    fix: progress display bug

--- resources/assets/js/page/Major/Statistic/ProgressList/index.js
+++ resources/assets/js/page/Major/Statistic/ProgressList/index.js
@@ -6,14 +6,15 @@ import { useSelector } from 'react-redux';
 import Progress from '@/components/Progress/index';
 
 function ProgressList() {
-    const { year, department, in_maj } = useSelector(
+    const { year, department, in_maj, category } = useSelector(
         (state) => state.major.filter
     );
     const average = useAverage();
     const min = useMin();
     const { passRate, request } = useFetchGAS(
         in_maj === 'none' ? department : in_maj,
-        year
+        year,
+        category
     );
 
     return (
@@ -22,8 +23,8 @@ function ProgressList() {
             <Progress name="最低錄取分數" value={min} />
             <Progress
                 name="通過率(官方數據)"
-                value={passRate * 100}
-                isLoading={passRate === undefined || request > 0}
+                value={passRate ? passRate * 100 : null}
+                isLoading={request > 0}
             />
         </ProgressListLayout>
     );
