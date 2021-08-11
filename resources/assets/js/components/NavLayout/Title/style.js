404541558b7d5805405f2f545587917dc3793190
JiaAnTW <at5lp6andy@gmail.com>
2021-08-11 21:36:40 +0800

    fix: adjust rwd issue

--- resources/assets/js/components/NavLayout/Title/style.js
+++ resources/assets/js/components/NavLayout/Title/style.js
@@ -4,10 +4,13 @@ export const H1 = styled.h1`
     margin: 0;
     font-size: 2.2rem;
     font-weight: 600;
+
+    @media (max-width: 576px) {
+        padding-left: 25px;
+    }
 `;
 
 export const Header = styled.header`
     display: grid;
-    grid-template-columns: auto 120px;
     height: 50px;
 `;
