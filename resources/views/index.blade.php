7f3885fc5855d3c1640e276a598d421b5d9db03c
JiaAnTW <at5lp6andy@gmail.com>
2021-11-07 19:43:07 +0800

    feat: init SSR comment

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html lang="zh-Hant-TW" charset="utf-8">
     <head>
-        <title>NCKU STUDY・成大學業分享</title>
+        <title>{{ $title }}</title>
         <!--setting-->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="theme-color" content="rgba(254, 218, 106, 1)">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="NCKU STUDY | 一個讓成大學生匿名分享轉系、輔修、雙主修...等各式學業心得的平台。希望以更透明的資訊幫助成大學生規劃自己學業的方向。(原NCKUTRANS)">
+        <meta name="description" content="{{ $description }}">
         <meta property="og:image" content="https://i.imgur.com/GJawE7A.jpg" />
         <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
         <link rel="icon" href="{{ asset('favicon.ico') }}">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use \App\Http\Controllers\CommentsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,8 +13,36 @@
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+Route::get('/', function (Request $request) {
+    $schoolName = trans('comment.schoolName');
+    $websiteTitleShort = trans('comment.websiteTitleShort'); 
+    $title = trans('comment.websiteTitle', [
+        'websiteTitleShort' => $websiteTitleShort,
+        'schoolName' => $schoolName,
+    ]);
+    $description = $title = trans('comment.description');
+
+    // For single essay url
+    $data = CommentsController::showById($request);
+    if($data){
+        $title = trans('comment.essayTitle', array(
+            'websiteTitle' => $websiteTitleShort,
+            'schoolName' => $schoolName,
+            'year' => $data['year'],
+            'in_maj' => $data['in_maj'],
+            'category' => $data['category']
+        ));
+        $description = str_replace("<br>","", $data['comment']); 
+    }
+    try {
+        if(mb_strlen($description) > 85) {
+            $description = mb_substr($description, 0, 85) . "......";
+        }
+    } catch(Exception $e){
+        error_log($e);
+    }
+
+    return view('index', compact('title', 'description'));
 });
 
 Route::get('/index', function () {
