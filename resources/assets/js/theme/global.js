c17368a6e075fe752fa03ae37a7021d60d4205c5
Fung Hei Man <94099670+fung-hei-man@users.noreply.github.com>
2021-12-13 08:37:09 +0800

    fix: scrolling scope of major page

--- resources/assets/js/theme/global.js
+++ resources/assets/js/theme/global.js
@@ -88,6 +88,28 @@ export const CardsContainer = styled.section`
     @media (max-width: 576px) {
         display: block;
     }
+
+    @media (min-width: 576px) {
+        overflow-y: auto;
+
+        ::-webkit-scrollbar {
+            width: 1px;
+            height: 1px;
+            background-color: transparent;
+            border: none;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            border-radius: 10px;
+            background-color: #555;
+            opacity: 0.5;
+        }
+
+        ::-webkit-scrollbar-track {
+            background-color: transparent;
+            border: 0px solid;
+        }
+    }
 `;
 
 export const WhiteContainer = styled.div`
@@ -117,6 +139,7 @@ const GlobalStyle = createGlobalStyle`
     color: ${color.darkBlack};
     font-size: 15px;
     line-height: 1.42857143;
+    overflow-y: hidden;
   }
 
   #root{
