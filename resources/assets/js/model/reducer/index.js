3b02f8afd043251f9348046e96d7f94f7212a8ee
JiaAnTW <at5lp6andy@gmail.com>
2021-12-19 15:18:21 +0800

    refactor: remove QA setting

--- resources/assets/js/model/reducer/index.js
+++ resources/assets/js/model/reducer/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import majorReducer from './major.js';
-import QAReducer from './qa.js';
 import studyReducer from './study.js';
 import departmentReducer from './department.js';
 import collegeReducer from './college.js';
@@ -11,7 +10,6 @@ import announceReducer from './announcement.js';
 
 export default combineReducers({
     major: majorReducer,
-    QA: QAReducer,
     study: studyReducer,
     in_maj: departmentReducer,
     college: collegeReducer,
