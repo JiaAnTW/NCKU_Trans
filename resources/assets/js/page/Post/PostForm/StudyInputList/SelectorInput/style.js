fa1af877b5d0bb8f09b35061d79a1d2d9c9317a7
Kyl23 <cklkailiang@gmail.com>
2022-01-19 22:48:40 +0800

    feat: selctor button different type style

--- resources/assets/js/page/Post/PostForm/StudyInputList/SelectorInput/style.js
+++ resources/assets/js/page/Post/PostForm/StudyInputList/SelectorInput/style.js
@@ -3,7 +3,9 @@ import { Button, color } from '~/theme/global';
 export const SelectorButton = styled(Button)`
     margin: 10px;
     border-radius: 30px;
-    background-color: ${color.white};
+    color: ${(props) => (props.selected ? color.black : color.yellow)};
+    background-color: ${(props) =>
+        props.selected ? color.yellow : color.white};
     border: 1px solid ${color.yellow};
     min-width: 80px;
     padding: 10px;
