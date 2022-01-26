e5ec1ef5fcab7bea9f7077d5eea838d07f5cb26b
Fung Hei Man <94099670+fung-hei-man@users.noreply.github.com>
2022-01-26 21:38:29 +0800

    refactor: rename item filter params & components

--- resources/assets/js/page/Study/Filter/ItemFilterEdit/index.js
+++ resources/assets/js/page/Study/Filter/ItemFilterEdit/index.js
@@ -10,33 +10,29 @@ import {
     FilterDisabledText,
 } from './style';
 
-const generateChildren = (arr, index) => {
-    return arr.type === 'year' ? (
-        <div>
-            <FilterEditDisabledContainer>
-                <FilterDisabledText>
-                    學年自動產生
-                    <br />
-                    無法編輯
-                </FilterDisabledText>
-            </FilterEditDisabledContainer>
-            <ItemFilterColumn
-                key={arr.type ? arr.type : index}
-                optionsArr={arr}
-            />
-        </div>
-    ) : (
-        <ItemFilterEditColumn optionsArr={arr} />
-    );
-};
-
 const ItemFilterEdit = forwardRef(() => {
     const filterObjArr = useItemFilter();
 
     return (
         <ItemFilterEditContainer>
             {map(filterObjArr, (arr, index) => {
-                return generateChildren(arr, index);
+                return arr.type === 'year' ? (
+                    <div>
+                        <FilterEditDisabledContainer>
+                            <FilterDisabledText>
+                                學年自動產生
+                                <br />
+                                無法編輯
+                            </FilterDisabledText>
+                        </FilterEditDisabledContainer>
+                        <ItemFilterColumn
+                            key={arr.type ? arr.type : index}
+                            optionsArr={arr}
+                        />
+                    </div>
+                ) : (
+                    <ItemFilterEditColumn optionsArr={arr} />
+                );
             })}
         </ItemFilterEditContainer>
     );
