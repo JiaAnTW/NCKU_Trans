84d6eea842ec0d0d542c102520db75c50a6c1dc8
vacantron <95543602+vacantron@users.noreply.github.com>
2022-01-29 12:55:59 +0800

    fix: fix useCallback dependencies

--- resources/assets/js/components/SearchBar/index.js
+++ resources/assets/js/components/SearchBar/index.js
@@ -4,14 +4,17 @@ import { InputField, SearchIconYellow, Button, Container } from './style';
 function SearchBar({ className, width, onSubmit }) {
     const [hidden, setHidden] = useState(false);
     const inputRef = useRef('');
-    const onFocus = () => setHidden(true);
-    const onBlur = () => setHidden(false);
-    const handleClick = useCallback((e) => {
-        e.preventDefault();
-        if (!inputRef.current.value) return;
+    const onFocus = useCallback(() => setHidden(true), []);
+    const onBlur = useCallback(() => setHidden(false), []);
+    const handleClick = useCallback(
+        (e) => {
+            e.preventDefault();
+            if (!inputRef.current.value) return;
 
-        onSubmit(inputRef.current.value);
-    }, []);
+            onSubmit(inputRef.current.value);
+        },
+        [onSubmit]
+    );
 
     return (
         <Container className={className} style={{ width }}>
