e945a2116901b4a5a55e1dd648cde45676d9bb4d
vacantron <95543602+vacantron@users.noreply.github.com>
2022-03-07 23:17:11 +0800

    fix: remove primary key in category

--- database/migrations/2022_01_20_154718_create__category_table.php
+++ database/migrations/2022_01_20_154718_create__category_table.php
@@ -9,7 +9,7 @@ class CreateCategoryTable extends BaseMigration
     public function __construct(){
         parent::setTableName('Category');
         parent::setCols([
-            'id' => function (Blueprint $table) { $table->uuid('id')->primary(); },
+            'id' => function (Blueprint $table) { $table->uuid('id'); },
             'name' => function (Blueprint $table) { $table->string('name',20); },
             'study_id' => function (Blueprint $table) { $table->uuid('study_id'); },
         ]);
